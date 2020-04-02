ac71ba6be159a80f0c2a34b7b15a19c88362466d
Swapnil Sankla <swapnil.sankla@gmail.com>
2020-04-02 11:44:13 +0530

    Send application number as uber-trace-id for distributed tracing across services

--- src/main/kotlin/com/swapnilsankla/loangateway/publisher/NeedLoanEventPublisher.kt
+++ src/main/kotlin/com/swapnilsankla/loangateway/publisher/NeedLoanEventPublisher.kt
@@ -2,6 +2,8 @@ package com.swapnilsankla.loangateway.publisher
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.swapnilsankla.loangateway.model.NeedLoanEvent
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.header.internals.RecordHeader
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
@@ -10,6 +12,16 @@ import org.springframework.stereotype.Component
 class NeedLoanEventPublisher(@Autowired val kafkaTemplate: KafkaTemplate<String, String>,
                              @Autowired val objectMapper: ObjectMapper) {
     fun publish(customerId: String, applicationNumber: String) {
-        kafkaTemplate.send("needLoanEvent", objectMapper.writeValueAsString(NeedLoanEvent(customerId, applicationNumber)))
+        kafkaTemplate.send(buildMessage(customerId, applicationNumber))
+    }
+
+    private fun buildMessage(customerId: String, applicationNumber: String): ProducerRecord<String, String> {
+        val message = ProducerRecord<String, String>(
+            "needLoanEvent",
+            objectMapper.writeValueAsString(NeedLoanEvent(customerId, applicationNumber))
+        )
+        message.headers().remove("uber-trace-id")
+        message.headers().add("uber-trace-id", applicationNumber.toByteArray())
+        return message
     }
 }
