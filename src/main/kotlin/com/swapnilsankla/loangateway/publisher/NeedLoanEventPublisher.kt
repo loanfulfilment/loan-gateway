492e1a05149be6ed76de34b8f44f9514b09a928d
Swapnil Sankla <swapnil.sankla@gmail.com>
2020-04-18 17:01:14 +0530

    Depend on starter to do kafka event publishing

--- src/main/kotlin/com/swapnilsankla/loangateway/publisher/NeedLoanEventPublisher.kt
+++ src/main/kotlin/com/swapnilsankla/loangateway/publisher/NeedLoanEventPublisher.kt
@@ -1,23 +1,13 @@
 package com.swapnilsankla.loangateway.publisher
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.swapnilsankla.loangateway.model.NeedLoanEvent
-import org.apache.kafka.clients.producer.ProducerRecord
+import com.swapnilsankla.tracestarter.CustomKafkaTemplate
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 
 @Component
-class NeedLoanEventPublisher(@Autowired val kafkaTemplate: KafkaTemplate<String, String>,
-                             @Autowired val objectMapper: ObjectMapper) {
+class NeedLoanEventPublisher(@Autowired val kafkaTemplate: CustomKafkaTemplate) {
     fun publish(customerId: String, applicationNumber: String) {
-        kafkaTemplate.send(buildMessage(customerId, applicationNumber))
-    }
-
-    private fun buildMessage(customerId: String, applicationNumber: String): ProducerRecord<String, String> {
-        return ProducerRecord(
-            "needLoanEvent",
-            objectMapper.writeValueAsString(NeedLoanEvent(customerId, applicationNumber))
-        )
+        kafkaTemplate.publish("needLoanEvent", NeedLoanEvent(customerId, applicationNumber))
     }
 }
